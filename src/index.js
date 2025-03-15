9864de72fddb95afcef266b8a748dcaa680eac91
Guilherme Messias <messiasguilherme700@gmail.com>
2025-03-15 14:02:36 -0300

    Refatora a função de formatação automática de texto, utilizando um módulo separado para melhorar a legibilidade e a manutenção do código

--- src/index.js
+++ src/index.js
@@ -1,30 +1,15 @@
-// Função de formatação de texto
+import { format } from "./format.js";
 
 const autoFormatButton = document.getElementById("autoFormatButton");
 autoFormatButton.addEventListener("click", () => {
-  try {
-    // Validação do texto inserido pelo usuário
-    const text = document.getElementById("textToFormat").value;
+  const textInput = document.getElementById("textToFormat").value;
 
-    // Remove acentos e cedilhas
-    const textWithoutAccents = text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  const textFormatted = format(textInput);
 
-    // Remove traços, pontos e barras
-    const textWithoutPunctuation = textWithoutAccents.replace(/[-./]/g, "");
+  document.getElementById("textToFormat").value = "";
 
-    // Converte para maiúsculas
-    const uppercaseText = textWithoutPunctuation.toUpperCase();
-
-    // Limpar Campos
-    document.getElementById("textToFormat").value = "";
-
-    // Apresentação do result formatado
-    const formattedText = document.getElementById("formattedText");
-    formattedText.innerText = uppercaseText;
-  } catch (error) {
-    alert("Erro ao processar text:", error);
-    // Exibir mensagem de erro para o usuário (opcional)
-  }
+  const formattedText = document.getElementById("formattedText");
+  formattedText.innerText = textFormatted;
 });
 
 // Função de copiar texto formatado para área de transferência
