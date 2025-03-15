9ebb2bea2e70d8f93a2abf8ced79efa231b4236c
Guilherme Messias <messiasguilherme700@gmail.com>
2025-03-15 14:36:44 -0300

    Adiciona limpeza do campo de entrada após formatação e ajusta a função de cópia de texto formatado

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import { format } from "./format.js";
 
 const autoFormatButton = document.getElementById("autoFormatButton");
+const copyFormattedTextButton = document.getElementById("copyFormattedTextButton");
+
 autoFormatButton.addEventListener("click", () => {
   const textInput = document.getElementById("textToFormat").value;
+  document.getElementById("textToFormat").value = "";
 
   const textFormatted = format(textInput);
-
-  document.getElementById("textToFormat").value = "";
 
   const formattedText = document.getElementById("formattedText");
   formattedText.innerText = textFormatted;
 });
 
-// Função de copiar texto formatado para área de transferência
-const copyFormattedTextButton = document.getElementById("copyFormattedTextButton");
 copyFormattedTextButton.addEventListener("click", () => {
   // Seleção do conteúdo formato apresentado na página
   const formattedText = document.getElementById("formattedText").textContent;
