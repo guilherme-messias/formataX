06b695977fbe324183954a14e2abd9a43804ed52
Guilherme Messias <messiasguilherme700@gmail.com>
2025-03-18 04:45:19 -0300

    Atualiza a função de formatação para aceitar novos parâmetros de configuração e melhorar a manipulação de texto

--- src/format.js
+++ src/format.js
@@ -1,10 +1,25 @@
-export function format(textInput) {
+export function format(
+  textInput,
+  isRemoveDots,
+  isUppercase,
+  isRemoveAccentsAndPunctuation,
+  isRemoveDashes
+) {
   try {
-    const textWithoutAccents = textInput.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-    const textWithoutPunctuation = textWithoutAccents.replace(/[-./]/g, "");
-    const uppercaseText = textWithoutPunctuation.toUpperCase();
-
-    return uppercaseText;
+    let textFormatted;
+    if (isRemoveDots) {
+      textFormatted = textInput.replace(/[.]/g, "");
+    }
+    if (isRemoveDashes) {
+      textFormatted = textFormatted.replace(/[-]/g, "");
+    }
+    if (isRemoveAccentsAndPunctuation) {
+      textFormatted = removeAccentsAndPunctuation(textFormatted);
+    }
+    if (isUppercase) {
+      textFormatted = textFormatted.toUpperCase();
+    }
+    return textFormatted;
   } catch (error) {
     alert("Erro ao processar text:", error);
   }
